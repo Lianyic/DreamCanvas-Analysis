a04d894dd474e84c6d9526a3bfc5c00357ecf768
lianyic <lilychen115@outlook.com>
2025-02-20 15:44:43 +0000

    update on app.py

--- app/app.py
+++ app/app.py
@@ -11,7 +11,8 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 app.config.update(
     SESSION_COOKIE_SAMESITE="None",
-    SESSION_COOKIE_SECURE=True
+    # SESSION_COOKIE_SECURE=True
+    SESSION_COOKIE_SECURE=False
 )
 
 # OpenAI API Key
